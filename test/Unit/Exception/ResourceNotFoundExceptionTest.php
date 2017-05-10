79855b3b6845d1d849e79887445094a46d27bea2
Andreas Möller <am@localheinz.com>
2017-05-10 21:51:56 +0200

    Fix: Cover exceptions

---
+++ test/Unit/Exception/ResourceNotFoundExceptionTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2017 Andreas Möller
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @link https://github.com/localheinz/github-pulse
+ */
+
+namespace Localheinz\GitHub\Pulse\Test\Unit\Exception;
+
+use Localheinz\GitHub\Pulse\Exception;
+use PHPUnit\Framework;
+use Refinery29\Test\Util\TestHelper;
+
+final class ResourceNotFoundExceptionTest extends Framework\TestCase
+{
+    use TestHelper;
+
+    public function testImplementsExceptionInterface(): void
+    {
+        $this->assertImplements(Exception\ExceptionInterface::class, Exception\ResourceNotFoundException::class);
+    }
+
+    public function testExtendsRuntimeException(): void
+    {
+        $this->assertExtends(\RuntimeException::class, Exception\ResourceNotFoundException::class);
+    }
+}
